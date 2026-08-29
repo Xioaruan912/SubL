b99b1b8531a0a34fa34133fd05b3e2546bd0a320
root <root@EE.localdomain>
2026-08-29 21:38:01 +0800

    build: include latest frontend assets

--- static/js/castArray.7z7I2Ah8.js
+++
@@ -1,1 +0,0 @@
-import{cd as r}from"./index.DYdnpGdK.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
--- static/js/castArray.BoddbXAg.js
+++
@@ -1,1 +0,0 @@
-import{cd as r}from"./index.D_BkOXHM.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
